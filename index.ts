8b95e6c327737821393f935a33c4d6573239e992
arjumandafreen <arjumandafreen407@gmail.com>
2024-04-12 03:39:22 -0700

    adding files of guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+const  answers = await inquirer.prompt([
+{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please Enter your guess number(Number limit from 1 to 7):",
+},
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+console.log("Congratulations! you guessed right number.");
+}
+else{
+console.log("Sorry you guessed wrong number");
+}
